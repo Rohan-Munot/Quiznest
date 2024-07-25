30704740a5111783ad9ed6d6307c8eff061eb03f
Rohan-Munot <rohanmunot24@gmail.com>
2024-07-25 12:55:15 +0530

    fe be connected

--- Frontend/src/Components/InputBox.jsx
+++ Frontend/src/Components/InputBox.jsx
@@ -1,12 +1,12 @@
 import styles from '../Pages/Frame.module.css';
 
-export function InputBox({label, placeholder, type}) {
+export function InputBox({label, placeholder, type, onChange}) {
     return (
 
             <div className={styles.inputFields}>
                 <div className={styles.title}>{label}</div>
                 <div>
-                    <input className={`${styles.inputFieldsParent} ${styles.placeholderTextWrapper}`} placeholder={placeholder} type={type}></input>
+                    <input onChange={onChange} className={`${styles.inputFieldsParent} ${styles.placeholderTextWrapper}`} placeholder={placeholder} type={type}></input>
                 </div>
             </div>
     )
--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -6,9 +6,16 @@ import {BottomWarning} from "../Components/BottomWarning.jsx";
 import {Heading} from "../Components/Heading.jsx";
 
 import styles from './Frame.module.css';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 
 export function Signup () {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
     return (
             <div className={styles.main}>
             <div className={styles.frameParent}>
@@ -23,15 +30,20 @@ export function Signup () {
                 </div>
                 <div className={styles.frameParent1}>
                     <div className={styles.inputFieldsParent}>
-                        <InputBox label={'Your Name'} placeholder={'Enter your full name'}/>
-                        <InputBox label={'Email ID'} placeholder={'Enter your email address'}/>
-                        <InputBox label={'Password'} placeholder={'Enter your password (min 6 characters)'}
-                                  type={'password'}/>
+                        <InputBox label={'Your Name'} placeholder={'Enter your full name'} onChange={e=>setFullName(e.target.value)}/>
+                        <InputBox label={'Email ID'} placeholder={'Enter your email address'} onChange={e=>setEmail(e.target.value)}/>
+                        <InputBox label={'Password'} placeholder={'Enter your password (min 6 characters)'} type={'password'} onChange={e=>setPassword(e.target.value)}/>
                         <InputBox label={'Confirm Password'} placeholder={'Re enter your password'} type={'password'}/>
                     </div>
                     <div className={styles.primaryButtonParent}>
-                        <Button label={'Register'} onClick={() => {
-                            console.log('register clicked')
+                        <Button label={'Register'} onClick={async () => {
+                            const response = await axios.post("http://localhost:3000/admin/signup", {
+                                fullname: fullName,
+                                username: email,
+                                password: password
+                            })
+                            localStorage.setItem("token", response.data.token)
+                            navigate('/dashboard')
                         }}/>
                         <BottomWarning to={'/signin'} label={'Already have an account?'} buttonText={'Login now'}/>
                     </div>
