a36e49185df6f324b5c8138a2e5e40cdf4d7d540
Rohan-Munot <rohanmunot24@gmail.com>
2024-07-26 13:42:25 +0530

    edit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Signup} from "./Pages/Signup.jsx";
+import {Signin} from "./Pages/Signin.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path="/signup" element={<Signup />} />
+              <Route path={'/signin'} element={<Signin />} />
           </Routes>
       </BrowserRouter>
     </div>
