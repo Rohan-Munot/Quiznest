3640aa4e2790c19f6e4f17765f79939d8d61eeb0
Rohan-Munot <rohanmunot24@gmail.com>
2024-07-31 22:37:23 +0530

    optimized

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Signup} from "./Pages/Signup.jsx";
 import {Signin} from "./Pages/Signin.jsx";
-import {Dashboard} from "./Pages/Dashboard.jsx";
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
           <Routes>
               <Route path="/signup" element={<Signup />} />
               <Route path={'/signin'} element={<Signin />} />
-              <Route path={'/dashboard'} element={<Dashboard />} />
           </Routes>
       </BrowserRouter>
     </div>
